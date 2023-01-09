5197a798b6dae1d954d02c63b446e3292ce97f71
tatiana pelozo <121576955+TATIANA-2000@users.noreply.github.com>
2023-01-09 13:46:47 -0300

    Proyecto Argentina Programa

---
+++ PROGRAMAS/PYTHON/archivo1.py
@@ -0,0 +1,14 @@
+print("¡primer ejercicio de informatorio!");
+print("Porfavor, ingrese dos numeros:");
+a=int(input("primer numero: "));
+b=int(input("segundo numero: "));
+print(a, "y", b, "¿Son iguales?", a == b);
+print(a, "y", b, "¿Son distintos?", a != b);
+print(a, "es mayor que ", b, ":", a>b);
+print(b, "es mayor o igual que", a, ":", b>=a);
+print("Segundo ejercicio:")
+print("El doble de cada numero", 2*a, "y", 2*b);
+print("Cada numero al cuadrado", a**2, "y", b**2); 
+print("La diferencia entre ambos", a-b)
+print("El resto de dividir", a, "/", b, ":", a%b);
+print("FIN DEL PROGRAMA");
